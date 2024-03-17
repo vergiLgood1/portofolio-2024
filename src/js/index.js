5da5532b854a5aff147b8455b2260c92c2ce07f3
Diyo Anggara <you@example.com git config --global user.email>
2024-03-18 00:13:54 +0700

    add sticky stack

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,20 @@
-const navBottom = document.getElementById('navBottom')
-const scrollText = document.getElementById('scrollText')
-
-navBottom.style.display = 'none'
+document.addEventListener('DOMContentLoaded', function() {
 
 
-const txts = document.querySelector(".animate-text").children,
+    const navBottom = document.getElementById('navBottom')
+    const scrollText = document.getElementById('scrollText')
+    
+    navBottom.style.display = 'none'
+    
+
+// Slide up animation text 
+const txts = document.querySelector(".slideUp-text").children,
     txtsLen = txts.length;
 let index = 0;
 const textInTimer = 3000,
     textOutTimer = 2800;
 
-function animateText() {
+function slideUp() {
     for (let i = 0; i < txtsLen; i++) {
         txts[i].classList.remove("text-in", "text-out");
     }
@@ -28,8 +32,26 @@ function animateText() {
         else {
             index++;
         }
-        animateText();
+        slideUp();
     }, textInTimer);
 }
 
-window.onload = animateText;
+window.onload = slideUp;
+
+
+// Fade up animation text
+window.addEventListener('scroll', function() {
+    const aboutContentRight = document.getElementById('about-content-right');
+    const sectionS2 = document.getElementById('s2');
+    const windowHeight = window.innerHeight;
+    const sectionTop = sectionS2.getBoundingClientRect().top;
+
+    if (sectionTop < windowHeight * 0.8) {
+      aboutContentRight.classList.add('fade-up-active');
+    }
+  });
+  
+
+
+})
+
