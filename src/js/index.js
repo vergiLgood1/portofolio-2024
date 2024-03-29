0eab8e7b1e06d4141dbcbcc5c606d3fa9bca8f5f
Diyo Anggara <xdamazon17@gmail.com>
2024-03-29 18:44:47 +0700

    Refactoring fungsi mouseEnter pada collection item

--- src/js/index.js
+++ src/js/index.js
@@ -80,141 +80,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // Buat sebuah fungsi untuk mengambil tinggi scroll halaman
-  // function getScrollHeight() {
-  //   return document.documentElement.scrollHeight || document.body.scrollHeight;
-  // }
 
-  // Panggil fungsi getScrollHeight() saat ingin mendapatkan tinggi scroll saat ini
-  // const scrollHeight = getScrollHeight();
-  // console.log("Tinggi scroll halaman:", scrollHeight);
 
-  // Tambahkan event listener untuk event scroll pada window
-  // window.addEventListener("scroll", () => {
-  //   const scrollHeight = getScrollHeight();
-  //   console.log("Tinggi scroll halaman:", scrollHeight);
-  // });
+// {Awal fungsi untuk menangani mouse hover pada collection item} -------------------------------->
 
-  // window.addEventListener("scroll", function (event) {
-  //   let scroll = window.scrollY;
-  //   console.log(scroll);
-  // });
-
-  // Change heigth on scroll event
-  // var content = document.getElementById("content");
-  // var contentMaxHeight = 200; // Tinggi maksimum yang ingin ditambahkan
-  // var initialScroll = window.pageYOffset; // Mengambil posisi scroll awal
-
-  // window.addEventListener("scroll", function () {
-  //   var currentScroll = window.pageYOffset;
-  //   var scrollDifference = currentScroll - initialScroll;
-
-  //   // Menghitung tinggi baru untuk div content
-  //   var newHeight = content.clientHeight + scrollDifference;
-
-  //   // Membatasi tinggi maksimum agar tidak melebihi contentMaxHeight
-  //   if (newHeight > contentMaxHeight) {
-  //     newHeight = contentMaxHeight;
-  //   }
-
-  //   content.style.height = newHeight + "px";
-
-  //   initialScroll = currentScroll; // Update posisi scroll awal
-  // });
-
-  // handle hover collection list item
-
-  // const collectionItem1 = document.getElementById("collection-item1");
-  // const collectionItem2 = document.getElementById("collection-item2");
-  // const collectionItem3 = document.getElementById("collection-item3");
-  // const collectionItem4 = document.getElementById("collection-item4");
-  // const collectionItem5 = document.getElementById("collection-item5");
-  // const collectionItem6 = document.getElementById("collection-item6");
-  // const collectionItem7 = document.getElementById("collection-item7");
-  // const iconSvg1 = document.getElementById("icon-svg1");
-  // const iconSvg2 = document.getElementById("icon-svg2");
-  // const iconSvg3 = document.getElementById("icon-svg3");
-  // const iconSvg4 = document.getElementById("icon-svg4");
-  // const iconSvg5 = document.getElementById("icon-svg5");
-  // const iconSvg6 = document.getElementById("icon-svg6");
-  // const iconSvg7 = document.getElementById("icon-svg7");
-  // const borderContact1 = document.getElementById("border-contact1");
-  // const borderContact2 = document.getElementById("border-contact2");
-  // const borderContact3 = document.getElementById("border-contact3");
-  // const borderContact4 = document.getElementById("border-contact4");
-  // const borderContact5 = document.getElementById("border-contact5");
-  // const borderContact6 = document.getElementById("border-contact6");
-  // const borderContact7 = document.getElementById("border-contact7");
-  // const textContact1 = document.getElementById("text-contact1");
-  // const textContact2 = document.getElementById("text-contact2");
-  // const textContact3 = document.getElementById("text-contact3");
-  // const textContact4 = document.getElementById("text-contact4");
-  // const textContact5 = document.getElementById("text-contact5");
-  // const textContact6 = document.getElementById("text-contact6");
-  // const textContact7 = document.getElementById("text-contact7");
-  // const vsCodeSrc = document.getElementById("vsCodeSrc");
-  // const figmaSrc = document.getElementById("figmaSrc");
-  // const aiSrc = document.getElementById("aiSrc");
-  
-  // collectionItem1.addEventListener("mouseenter", () => {
-  //   iconSvg1.style.borderColor = "#ffff";
-  //   vsCodeSrc.src = "/src/assets/svg-content/vsCode2.svg";
-  //   borderContact1.style.borderColor = "#ffffff";
-  //   borderContact1.style.backgroundColor = "#ffffff";
-  //   textContact1.style.color = "#000000";
-  // });
-
-  // collectionItem1.addEventListener("mouseleave", () => {
-  //   iconSvg1.style.borderColor = "#000";
-  //   vsCodeSrc.src = "/src/assets/svg-content/vsCode.svg";
-  //   borderContact1.style.borderColor = "#000";
-  //   borderContact1.style.backgroundColor = "none";
-  //   textContact1.style.color = "#000";
-  // });
-
-  // collectionItem2.addEventListener("mouseenter", () => {
-  //   iconSvg2.style.borderColor = "#ffff";
-  //   borderContact2.style.borderColor = "#ffffff";
-  //   borderContact2.style.backgroundColor = "#ffffff";
-  //   textContact2.style.color = "#000000";
-  // });
-
-  // collectionItem2.addEventListener("mouseleave", () => {
-  //   iconSvg2.style.borderColor = "#000";
-  //   borderContact2.style.borderColor = "#000";
-  //   borderContact2.style.backgroundColor = "none";
-  //   textContact2.style.color = "#000";
-  // });
-
-  // collectionItem3.addEventListener("mouseenter", () => {
-  //   iconSvg3.style.borderColor = "#ffff";
-  //   borderContact3.style.borderColor = "#ffffff";
-  //   borderContact3.style.backgroundColor = "#ffffff";
-  //   textContact3.style.color = "#000000";
-  // });
-
-  // collectionItem3.addEventListener("mouseleave", () => {
-  //   iconSvg3.style.borderColor = "#000";
-  //   borderContact3.style.borderColor = "#000";
-  //   borderContact3.style.backgroundColor = "none";
-  //   textContact3.style.color = "#000";
-  // });
-
-  // collectionItem4.addEventListener("mouseenter", () => {
-  //   iconSvg4.style.borderColor = "#ffff";
-  //   borderContact4.style.borderColor = "#ffffff";
-  //   borderContact4.style.backgroundColor = "#ffffff";
-  //   textContact4.style.color = "#000";
-  // });
-
-  // collectionItem4.addEventListener("mouseleave", () => {
-  //   iconSvg4.style.borderColor = "#000";
-  //   borderContact4.style.borderColor = "#000";
-  //   borderContact4.style.backgroundColor = "none";
-  //   textContact4.style.color = "#000";
-  // });
-
-  // Menyiapkan objek yang menyimpan referensi ke elemen-elemen yang dibutuhkan
+// Menyiapkan objek yang menyimpan referensi ke elemen-elemen yang dibutuhkan
 const elements = {
   collectionItems: document.querySelectorAll("[id^='collection-item']"),
   iconSvgs: document.querySelectorAll("[id^='icon-svg']"),
@@ -232,7 +102,7 @@ function mouseEnter(event) {
   elements.iconSvgs[index - 1].style.borderColor = "#ffff";
   elements.borderContacts[index - 1].style.borderColor = "#ffffff";
   elements.borderContacts[index - 1].style.backgroundColor = "#ffffff";
-  elements.textContacts[index - 1].style.color = "#000000";
+  elements.textContacts[index - 1].style.color = "#051118";
 
   // Mengubah src iconSvg berdasarkan ID elemen
   switch (event.target.id) {
@@ -267,10 +137,10 @@ function mouseleave(event) {
   const index = event.target.id.slice(-1); // Mendapatkan indeks dari ID elemen
 
   // Memperbarui gaya sesuai dengan indeks
-  elements.iconSvgs[index - 1].style.borderColor = "#000";
-  elements.borderContacts[index - 1].style.borderColor = "#000";
+  elements.iconSvgs[index - 1].style.borderColor = "#051118";
+  elements.borderContacts[index - 1].style.borderColor = "#051118";
   elements.borderContacts[index - 1].style.backgroundColor = "transparent";
-  elements.textContacts[index - 1].style.color = "#000";
+  elements.textContacts[index - 1].style.color = "#051118";
 
   // Mengubah src iconSvg berdasarkan ID elemen
   switch (event.target.id) {
@@ -307,5 +177,6 @@ elements.collectionItems.forEach((item) => {
   item.addEventListener("mouseleave", mouseleave);
 });
 
+// <-------------------------------- {Akhir fungsi untuk menangani mouse hover pada collection item} 
   
 });
