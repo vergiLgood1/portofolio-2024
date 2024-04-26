67d08a5befc8c635cb30f5f44f180cb9cdf4561f
Diyo Anggara <xdamazon17@gmail.com>
2024-04-26 23:57:20 +0700

    add loader v1

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -3,6 +3,46 @@ import gsap from "gsap";
 document.addEventListener("DOMContentLoaded", function () {
   gsap.registerPlugin(ScrollTrigger);
 
+  gsap.set(".img", { y: 500 });
+  gsap.set(".loader-imgs", { x: 500 });
+
+  const tl = gsap.timeline({ delay: 1 });
+
+  tl.to(".img", {
+    y: 0,
+    duration: 1.5,
+    stagger: 0.05,
+    ease: "power3.inOut",
+  })
+    .to(
+      ".loader-imgs",
+      {
+        x: 0,
+        duration: 3,
+        ease: "power3.inOut",
+      },
+      "-=2.5"
+    )
+    .to(
+      ".img:not(#loader-logo",
+      {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+        duration: 1,
+        stagger: 0.1,
+        ease: "power3.inOut",
+      },
+      "-=1"
+    )
+    .to(
+      ".loader",
+      {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+        duration: 1,
+        ease: "power3.inOut",
+      },
+      "-=0.5"
+    );
+
   gsap.to(".main-cta-row", {
     scrollTrigger: {
       trigger: ".main-cta-row",
@@ -98,5 +138,4 @@ document.addEventListener("DOMContentLoaded", function () {
       gsap.set(".footer", { opacity: ScrollTrigger.progress });
     },
   });
-
 });
